92b17981af72976ac71aa49f0d31a1061c721b48
Gabriel Guerreiro <136819754+GabrielGuerreir0@users.noreply.github.com>
2025-11-22 22:13:52 -0300

    import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { HeaderContainer } from "./styles";
 export function Header() {
   return (
     <HeaderContainer>
-      <img src="logo.png" alt="Logo" />
+      <img src="logo.png" alt="flytimer logo" />
       <nav>
         <a href="">timer</a>
         <a href="">history</a>
